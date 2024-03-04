878bb08da12054d29909ce8256837e8fc35e5c4d
Sakshham Bhagat <sakshhamtg@gmail.com>
2024-03-04 23:25:52 +0530

    Permissions changed

--- lib/utils/api_calls.dart
+++ lib/utils/api_calls.dart
@@ -66,28 +66,3 @@ class APICalls {
     return file;
   }
 }
-
-Future<List> fetchPYQs(courseCode) async {
-  final List pyqs = [];
-  final response = await MyHttpClient().post('https://cl.thapar.edu/view1.php', {'ccode': courseCode, 'submit': ''});
-  if (response.statusCode == 200) {
-    final document = parse(response.body);
-    final table = document.getElementsByTagName('table');
-    final rows = table[0].getElementsByTagName('tr');
-    for (var i = 0; i < rows.length; i++) {
-      if (i == 0 || i == 1) continue;
-      final cells = rows[i].getElementsByTagName('td');
-      pyqs.add({
-        'code': cells[0].text,
-        'name': cells[1].text,
-        'year': cells[2].text,
-        'semester': cells[3].text,
-        'type': cells[4].text,
-        'link': cells[5].getElementsByTagName('a')[0].attributes['href'],
-      });
-    }
-    return pyqs;
-  } else {
-    throw Exception('Failed to fetch PYQs');
-  }
-}
